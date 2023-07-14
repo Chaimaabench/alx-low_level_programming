08c6a6beeaa9eaa1b093daf5f8a9e70d4edd0c99
chaimaabench <chaimaabencheqroun@gmail.com>
2023-07-14 14:40:02 -0700

    task

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	char i;
+	char j;
+
+	i='0';
+	j='a';
+	
+	for (; i <= '9'; i++)
+	{
+		putchar(i);
+	}
+	for (; j <= 'f'; j++)
+	{
+		putchar(j);
+	}
+	printf("\n");
+	return (0);
+}
