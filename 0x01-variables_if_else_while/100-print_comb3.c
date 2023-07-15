91dfb290e927886fad7ae5ed4987473bcfe0c039
chaimaabench <chaimaabencheqroun@gmail.com>
2023-07-14 17:41:46 -0700

    ast

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -3,28 +3,30 @@
  * main - Entry point
  * 
  * Return: Always 0 (succeed)
-*/
+ */
 int main(void)
 {
-    int ones = '0';
-    int tens = '0';
+	int ones = '0';
+	int tens = '0';
 
-    for (tens = '0'; tens <= '9'; tens++)
-    {
-        for (ones = '0'; ones <= '9'; ones++){
-             if(!((ones == tens) || (tens > ones)))
-             {
-                putchar(tens);
-                putchar(ones);
-                if (!(ones == '9' && tens == '8'))
-                {
-                    putchar(',');
-                    putchar(' ');
-                }
-             }
-        }
-    }
+	for (tens = '0'; tens <= '9'; tens++)
+	{
+		for (ones = '0'; ones <= '9'; ones++)
+		{
+			if (!((ones == tens) || (tens > ones)))
+			{
+				putchar(tens);
+				putchar(ones);
 
-    putchar('\n');
-    return (0);
+				if (!(ones == '9' && tens == '9'))
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+
+	putchar('\n');
+	return (0);
 }
