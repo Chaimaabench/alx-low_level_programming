0c8ad18fa586bb17affa3e180c1920c15c82574d
chaimaabench <chaimaabencheqroun@gmail.com>
2023-07-18 15:36:10 -0700

    as

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,13 +2,14 @@
 
 /**
  * main - check the code
- * print alphabet in lower case
+ * print_alphabet - to print alphabet in lower case
  * Return: Always 0 (success)
  */
 
 void print_alphabet(void)
 {
 	char az;
+
 	az = 'a';
 	while (az <= 'z')
 	{
