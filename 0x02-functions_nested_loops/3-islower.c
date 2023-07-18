5cd1a9ee9679d9f0ee7383dbaf0c823514aa181c
chaimaabench <chaimaabencheqroun@gmail.com>
2023-07-18 16:13:29 -0700

    as

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * desc int _islower(int c) - checks for lowercase char
+ *
+ * return 1 if c lowercase or else 0
+ */
+
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+	{
+	return (1);
+	}
+	return (0);
+}
