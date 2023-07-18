4737c25f3cfe750643c5cf9c71e7161f6fadac57
chaimaabench <chaimaabencheqroun@gmail.com>
2023-07-18 15:55:17 -0700

    as

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - to print alphabet in lower case ten times
+ *
+ * Return: Always 0 (success)
+ */
+
+void print_alphabet_x10(void)
+{
+	char az;
+	int i;
+
+	for (; i <= 10; i++)
+	{
+		az = 'a';
+		while (az <= 'z')
+		{
+		_putchar(az);
+		az++;
+		}
+		_putchar('\n');
+	}
+}
