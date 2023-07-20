1717624734d91e43ae20b722868a6a304e4269f2
chaimaabench <chaimaabencheqroun@gmail.com>
2023-07-20 04:05:24 -0700

    aqs

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+* mul -  multiples two integers and returns the result.
+* @a: the first number
+* @b: the second number
+* Return: Integer value
+*/
+int mul(int a, int b)
+{
+	return (a * b);
+}
---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_most_numbers - prints the numbers, from 0 to 9, followed by a new line
+ *
+ * Return: Always 0 (success)
+ */
+
+void print_most_numbers(void)
+{
+		char i;
+
+		i = '0';
+
+		for (; i <= '9'; i++)
+		{
+			if(i == '2' || i == '4')
+			{
+				continue;
+			}
+			_putchar(i);
+		}
+
+		_putchar('\n');
+}
