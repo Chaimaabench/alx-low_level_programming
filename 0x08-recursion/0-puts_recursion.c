02ca14645770cd761a1a6a9ac8841a45318fc781
chaimaabench <chaimaabencheqroun@gmail.com>
2023-08-02 13:56:30 -0700

    as

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * _puts_recursion -  prints a string, followed by a new line.
+ * @c: character to be printed
+ * Return: 1 if success and -1 if error
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+	else
+	{
+		_putchar('/0')
+	}
+}
