fe95a91ff906aa32895dd50d832ca28402cc98db
chaimaabench <chaimaabencheqroun@gmail.com>
2023-08-03 13:29:43 -0700

    ask

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,68 @@
+#include "main.h"
+
+/**
+ * move_past_star - iterates past asterisk
+ * @s2: second string
+ *
+ * Return: pointer past star
+ */
+
+char *move_past_star(char *s2)
+{
+	if (*s2 == '*')
+		return (move_past_star(s2 + 1));
+	else
+		return (s2);
+}
+
+/**
+ * inception - checks if identical or not
+ * @s1: first string
+ * @s2: second string
+ * Return: 1 if identical, 0 if false
+ */
+int inception(char *s1, char *s2)
+{
+	int re = 0;
+
+	if (*s1 == 0)
+		return (0);
+	if (*s1 == *s2)
+		re += wildcmp(s1 + 1, s2 + 1);
+	re += inception(s1 + 1, s2);
+	return (re);
+}
+
+/**
+ * wildcmp - compares two strings
+ * @s1: pointer to the first string
+ * @s2: pointer to the second string
+ * Return: 1 if identical or 0 if error
+ */
+
+int wildcmp(char *s1, char *s2)
+{
+	int re = 0;
+
+	if (!*s1 && *s2 == '*' && !*move_past_star(s2))
+		return (1);
+	if (*s1 == *s2)
+	{
+		if (!*s1)
+			return (1);
+		return (wildcmp(s1 + 1, *s2 == '*' ? s2 : s2 + 1));
+	}
+	if (!*s1 || !s2)
+		return (0);
+	if (*s2 == '*')
+	{
+		s2 = move_past_star(s2);
+		if (!*s2)
+			return (1);
+		if (*s1 == *s2)
+			re += wildcmp(s1 + 1, s2 + 1);
+		re += inception(s1, s1);
+		return (!!re);
+	}
+	return (0);
+}
---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * is_prime_number - check if integer is a prime number
+ * @n: integer
+ * Return: int
+ */
+int check_prime(int n, int n2);
+int is_prime_number(int n)
+{
+return (check_prime(n, 2));
+}
+
+/**
+ * check_prime - check all numbers of < n
+ * @n: integer
+ * @n2: integer
+ * Return: int
+ */
+
+int check_prime(int n, int n2)
+{
+	if (n2 >= n && n > 1)
+		return (1);
+	else if (n % n2 == 0 || n <= 1)
+		return (0);
+	else
+		return (check_prime(n, n2 + 1));
+}
